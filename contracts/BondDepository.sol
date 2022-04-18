b8b241e3969a5a19d67ae70a6aede0dac11fce69
HyukSang <zzooppii@gmail.com>
2022-04-18 22:35:50 +0900

    TSV2-74_basic libraries
    libraries

---
+++ contracts/BondDepository.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.10;
+
+import "./libraries/SafeERC20.sol";
+
+
+contract BondDepository {
+    using SafeERC20 for IERC20;
+
+
+}
