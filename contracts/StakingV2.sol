209b9e430ed22cb2c74f9164b719b3a485c63572
zena <zena.p@onther.io>
2022-09-26 13:50:27 +0900

    TS2-6. Potential Lose of LTOS

--- contracts/StakingV2.sol
+++ contracts/StakingV2.sol
@@ -186,7 +186,25 @@ contract StakingV2 is
 
         rebaseIndex();
 
-        _createStakeInfo(msg.sender, stakeId, _amount, block.timestamp + 1, 0);
+        uint256 ltos = getTosToLtos(_amount);
+
+        if (allStakings[stakeId].staker == msg.sender) {
+            LibStaking.UserBalance storage _stakeInfo = allStakings[stakeId];
+            _stakeInfo.deposit += _amount;
+            _stakeInfo.ltos += ltos;
+
+        } else {
+            allStakings[stakeId] = LibStaking.UserBalance({
+                staker: msg.sender,
+                deposit: _amount,
+                ltos: ltos,
+                endTime: block.timestamp + 1,
+                marketId: 0
+            });
+        }
+
+        stakingPrincipal += _amount;
+        totalLtos += ltos;
 
         emit Staked(msg.sender, _amount, stakeId);
     }
