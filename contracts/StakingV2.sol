adbc343a8e2deb5be784288eb8c934dc08ba10d5
zena <zena.p@onther.io>
2022-11-01 12:20:18 +0900

    TSV2-133. fix bug in increaseBeforeEndOrNonEnd
    when n is 0 , increaseAmountByStaker is not executed.

--- contracts/StakingV2.sol
+++ contracts/StakingV2.sol
@@ -429,7 +429,7 @@ contract StakingV2 is
         totalLtos += ltos;
 
         uint256 lockId = connectId[_stakeId];
-        uint256 amountCompound = 0;
+        uint256 amountCompound = _amount;
         if (userStakingIndex[msg.sender][_stakeId] > 1 && lockId > 0) {
             (, uint256 end, uint256 principal) = ILockTosV2(lockTOS).locksInfo(lockId);
             require(end > block.timestamp && _stakeInfo.endTime > block.timestamp, "lock end time has passed");
@@ -438,10 +438,8 @@ contract StakingV2 is
             if (n == 1) amountCompound = _amount * (1 ether + rebasePerEpoch) / 1e18;
             else if (n > 1) amountCompound = LibStaking.compound(_amount, rebasePerEpoch, n);
 
-            if (amountCompound > 0){
-                ILockTosV2(lockTOS).increaseAmountByStaker(msg.sender, lockId, amountCompound);
-                amountCompound = principal + amountCompound;
-            }
+            ILockTosV2(lockTOS).increaseAmountByStaker(msg.sender, lockId, amountCompound);
+            amountCompound = principal + amountCompound;
         }
         emit IncreasedBeforeEndOrNonEnd(msg.sender, _amount, 0, _stakeId, lockId, amountCompound);
     }
