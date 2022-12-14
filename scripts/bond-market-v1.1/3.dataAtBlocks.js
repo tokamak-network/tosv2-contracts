2a9f20603f639455a019ae6164831bd22c7b9c96
zena <zena.p@onther.io>
2022-12-14 12:34:32 +0900

    Update 3.dataAtBlocks.js

--- scripts/bond-market-v1.1/3.dataAtBlocks.js
+++ scripts/bond-market-v1.1/3.dataAtBlocks.js
@@ -22,13 +22,17 @@ async function main() {
     // let bondDepositoryProxyAbi = require('../../artifacts/contracts/BondDepositoryProxy.sol/BondDepositoryProxy.json');
     let stakingV2Abi = require('../../artifacts/contracts/StakingV2.sol/StakingV2.json');
     let treasuryAbi = require('../../artifacts/contracts/Treasury.sol/Treasury.json');
-
+    let lockTOSAbi = require('../../abis/LockTOSv2Logic0.json');
+    //totalSupply
 
     const stakingV2Address = "0x14fb0933Ec45ecE75A431D10AFAa1DDF7BfeE44C";
     const treasuryAddress = "0xD27A68a457005f822863199Af0F817f672588ad6";
+    const lockTOsAddress = "0x69b4A202Fa4039B42ab23ADB725aA7b1e9EEBD79";
     console.log("stakingV2Address",stakingV2Address)
     console.log("treasuryAddress",treasuryAddress)
+    console.log("lockTOsAddress",lockTOsAddress)
 
+    /*
     // const bondDepositoryProxyContract = new ethers.Contract(bondDepositoryProxyAddress, bondDepositoryProxyAbi.abi, ethers.provider);
     const stakingV2Contract = new ethers.Contract(stakingV2Address, stakingV2Abi.abi, ethers.provider);
     const treasuryContract = new ethers.Contract(treasuryAddress, treasuryAbi.abi, ethers.provider);
@@ -38,6 +42,13 @@ async function main() {
     let enableStaking = await treasuryContract.enableStaking();
     let rebasePerEpoch = await stakingV2Contract.rebasePerEpoch();
     console.log( totalLtos.toString() ,", ", possibleIndex.toString() ,", ", enableStaking.toString() ,", ", rebasePerEpoch.toString() );
+    */
+
+    const lockTOSContract = new ethers.Contract(lockTOsAddress, lockTOSAbi.abi, ethers.provider);
+
+    let totalSupply = await lockTOSContract.totalSupply();
+    console.log( totalSupply.toString() );
+
 
 }
 
