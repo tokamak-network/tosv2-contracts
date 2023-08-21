b98397be782a6db595f286bfc5ec8d9dee3dfa0e
zena <zena@tokamak.network>
2023-08-21 11:01:36 +0900

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -34,6 +34,10 @@ module.exports = {
   live: false,
   networks: {
     hardhat: {
+      forking: {
+        url: `${process.env.ETH_NODE_URI_MAINNET}`,
+        blockNumber: 17923675
+      },
       chainId: 31337,
     },
     local: {
