f31c6f133ff92d80bc79938eec4a6e318ed6c93c
HyukSang <zzooppii@gmail.com>
2024-02-05 15:27:06 +0900

    StakeVault + TOS mining update

--- scripts/check_tos/check_TOS.js
+++ scripts/check_tos/check_TOS.js
@@ -20,6 +20,12 @@ async function main() {
     let foundation3 = "0x2520CD65BAa2cEEe9E6Ad6EBD3F45490C42dd303"
     let DAOVault = "0x2520CD65BAa2cEEe9E6Ad6EBD3F45490C42dd303"
     let wtonTosPool = "0x1c0cE9aAA0c12f53Df3B4d8d77B82D6Ad343b4E4"
+    let StakeVault = "0xf04f6A6D6115D8400D18ECa99BdEe67ABB498a7B"
+    let tosmining1 = "0x21Db1777Dd95749A849d9e244136E72bd93082Ea"
+    let tosmining2 = "0x9F97b34161686d60ADB955ed63A2FC0b2eC0a2a9"
+    let tosmining3 = "0xFC1fC3a05EcdF6B3845391aB5CF6a75aeDef7CeA"
+    let tosmining4 = "0x7da4E8Ab0bB29a6772b6231b01ea372994c2A49A"
+    let tosmining5 = "0x9a8294566960Ab244d78D266FFe0f284cDf728F1"
     
     const TOSContract = new ethers.Contract( TOSAddr, tosABI.abi, ethers.provider);
 
@@ -29,6 +35,13 @@ async function main() {
     let foundation3TOS = await TOSContract.balanceOf(foundation3);
     let DAOVaultTOS = await TOSContract.balanceOf(DAOVault);
     let wtonTosPoolTOS = await TOSContract.balanceOf(wtonTosPool);
+    let stakeVaultTOS = await TOSContract.balanceOf(StakeVault);
+    let tosmining1TOS = await TOSContract.balanceOf(tosmining1);
+    let tosmining2TOS = await TOSContract.balanceOf(tosmining2);
+    let tosmining3TOS = await TOSContract.balanceOf(tosmining3);
+    let tosmining4TOS = await TOSContract.balanceOf(tosmining4);
+    let tosmining5TOS = await TOSContract.balanceOf(tosmining5);
+
 
     let ethTreasury = ethers.utils.formatEther(TreasuryTOS);
     let ethFoundation1 = ethers.utils.formatEther(foundation1TOS);
@@ -36,6 +49,12 @@ async function main() {
     let ethFoundation3 = ethers.utils.formatEther(foundation3TOS);
     let ethDAOVault = ethers.utils.formatEther(DAOVaultTOS);
     let ethwtonTosPoolTOS = ethers.utils.formatEther(wtonTosPoolTOS);
+    let ethStakeVaultTOS = ethers.utils.formatEther(stakeVaultTOS);
+    let ethtosmining1TOS = ethers.utils.formatEther(tosmining1TOS);
+    let ethtosmining2TOS = ethers.utils.formatEther(tosmining2TOS);
+    let ethtosmining3TOS = ethers.utils.formatEther(tosmining3TOS);
+    let ethtosmining4TOS = ethers.utils.formatEther(tosmining4TOS);
+    let ethtosmining5TOS = ethers.utils.formatEther(tosmining5TOS);
     
 
     console.log("Treasury TOS blanaces :", ethTreasury, " TOS");
@@ -44,6 +63,12 @@ async function main() {
     console.log("Foundation3 TOS blanaces :", ethFoundation3, " TOS");
     console.log("DAOVault TOS blanaces :", ethDAOVault, " TOS");
     console.log("WTON-TOS Pool TOS blanaces :", ethwtonTosPoolTOS, " TOS");
+    console.log("StakeVault TOS blanaces :", ethStakeVaultTOS, " TOS");
+    console.log("TOSMining1 TOS blanaces :", ethtosmining1TOS, " TOS");
+    console.log("TOSMining2 TOS blanaces :", ethtosmining2TOS, " TOS");
+    console.log("TOSMining3 TOS blanaces :", ethtosmining3TOS, " TOS");
+    console.log("TOSMining4 TOS blanaces :", ethtosmining4TOS, " TOS");
+    console.log("TOSMining5 TOS blanaces :", ethtosmining5TOS, " TOS");
 
 }
 
